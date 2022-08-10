a15101ad2a5296b551812d83d29e7039e642ed9a
MBB200291 <MBB200291@users.noreply.github.com>
2022-08-10 19:24:10 +0800

    imp: imporve efficiency by recording the surround living cell count

--- myscript.js
+++ myscript.js
@@ -4,24 +4,36 @@ window.addEventListener("load", function (event) {
   const numSeat = 12000;
   const colSize = 150;
   let rowSize = numSeat / colSize;
-  const SPARSE = 2;
-  const DENSE = 5; //4;
-  const REPRODUCE = 2; //3;
-  const SPARSE2 = 1;
-  const DENSE2 = 4;
-  const REPRODUCE2 = 3;
+  const SPARSE = 1;
+  const DENSE = 4;
+  const REPRODUCE = 3;
   const RANDOM = false;
-  const RANDOMPROB = 0.001;
+  const RANDOMPROB = 0.0001;
   const midSection = document.getElementById("seating");
   const roundSpan = document.getElementById("round");
-  const initialStates = //"";
-    "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011111100000000000011111111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011111100000000000011111111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011111100000000000011111111";
-  //
-  let states = [];
-  let statesTemp;
-  function createSeatLayout() {
+  const initialStates =
+    "110001001001001001110001011100010010010010011011100010010010010011001001001001110001000101110001001001001001101110001001001001001001110001001001001001110001001001001001110001001001001001110001001000111000100100100100111001001110001000101110001001001001001101110001001001001001001";
+
+  let liveStates = []; // keeping the count of neighbor living cell
+  // let statesTemp = []; // keeping the count of neighbor living cell
+  let countStates = []; // keeping the count of neighbor living cell
+  let countStatesTemp = []; // keeping the count of neighbor living cell
+  function createGrid() {
+    let grid = [];
     for (let r = 0; r < rowSize; r++) {
-      states.push([]);
+      grid.push([]);
+      for (let c = 0; c < colSize; c++) {
+        grid[r].push(0);
+      }
+    }
+    return grid;
+  }
+
+  function createSeatLayout() {
+    liveStates = createGrid();
+    countStates = createGrid();
+    countStatesTemp = createGrid();
+    for (let r = 0; r < rowSize; r++) {
       for (let c = 0; c < colSize; c++) {
         // console.log(r, c);
         var initialState =
@@ -29,19 +41,123 @@ window.addEventListener("load", function (event) {
             ? 0
             : Number(initialStates[r * colSize + c]);
         midSection.appendChild(createSeat(r, c, initialState));
-        states[r].push(initialState);
+        liveStates[r][c] = initialState;
+        if (initialState === 1) {
+          updateCount(r, c);
+        }
       }
     }
-    statesTemp = JSON.parse(JSON.stringify(states));
   }
+
   function createSeat(row, col, initialState) {
     let seat = document.createElement("div");
     seat.setAttribute("id", `${row}_${col}`);
     if (initialState === 0) seat.setAttribute("class", "a");
     else if (initialState === 1) seat.setAttribute("class", "s");
-    else if (initialState === 2) seat.setAttribute("class", "r");
+    // seat.innerText = `${row}_${col}`;
     seat.innerText = "";
     return seat;
+  }
+
+  function updateState() {
+    for (let r = 0; r < rowSize; r++) {
+      // statesTemp.push([]);
+      for (let c = 0; c < colSize; c++) {
+        let liveState = checkState(r, c);
+        liveStates[r][c] = liveState;
+        if (liveState === 1) updateCount(r, c);
+        // liveStates[r].push(checkState(r, c));
+        // if (RANDOM && Math.random() < RANDOMPROB) {
+        //   statesTemp[r][c] = 1 - statesTemp[r][c];
+        // }
+      }
+    }
+  }
+
+  function checkState(r, c) {
+    if (liveStates[r][c] === 1) {
+      if (countStates[r][c] <= SPARSE) return 0;
+      else if (countStates[r][c] >= DENSE) return 0;
+      else return 1;
+    } else {
+      if (countStates[r][c] === REPRODUCE) return 1;
+      else return 0;
+    }
+  }
+  function updateCount(r, c, value = 1) {
+    for (let i = r - 1; i <= r + 1; i++) {
+      for (let j = c - 1; j <= c + 1; j++) {
+        if (
+          // i < 0 ||
+          // j < 0 ||
+          // i >= rowSize - 1 ||
+          // j >= colSize - 1 ||
+          i === r &&
+          j === c
+        ) {
+          continue;
+        }
+        let it = i < 0 ? rowSize - 1 : i >= rowSize ? 0 : i;
+        let jt = j < 0 ? colSize - 1 : j >= colSize ? 0 : j;
+        countStatesTemp[it][jt] += value;
+      }
+    }
+  }
+  // function checkState(r, c) {
+  //   let survived = 0;
+  //   for (let i = r - 1; i <= r + 1; i++) {
+  //     for (let j = c - 1; j <= c + 1; j++) {
+  //       if (
+  //         // i < 0 ||
+  //         // j < 0 ||
+  //         // i >= rowSize - 1 ||
+  //         // j >= colSize - 1 ||
+  //         i === r &&
+  //         j === c
+  //       ) {
+  //         continue;
+  //       }
+  //       let it = i < 0 ? rowSize - 1 : i >= rowSize ? 0 : i;
+  //       let jt = j < 0 ? colSize - 1 : j >= colSize ? 0 : j;
+  //       // console.log(i, j);
+  //       if (states[it][jt] === 1) survived++;
+  //     }
+  //   }
+  //   if (states[r][c] === 1) {
+  //     // console.log(`${r}_${c} survived: ${survived}`);
+  //     // rule 1: dead if near-by live cell <= 1 (SPARSE)
+  //     if (survived <= SPARSE) return 0;
+  //     // rule 2: dead if near-by live cell >= 4 (DENSE)
+  //     else if (survived >= DENSE) return 0;
+  //     else return 1;
+  //   } else {
+  //     // rule 3: alive if near-by live cell == 3 (REPRODUCE)
+  //     if (survived === REPRODUCE) return 1;
+  //     else return 0;
+  //   }
+  // }
+
+  function renderPage() {
+    for (let r = 0; r < rowSize; r++) {
+      for (let c = 0; c < colSize; c++) {
+        if (liveStates[r][c] === 1) {
+          document.getElementById(`${r}_${c}`).className = "s";
+        } else {
+          document.getElementById(`${r}_${c}`).className = "a";
+        }
+      }
+    }
+  }
+
+  // core
+  function gameRound() {
+    // update state
+    updateState();
+    // render
+    renderPage();
+    // save temp to state
+    countStates = countStatesTemp;
+    countStatesTemp = createGrid();
   }
 
   // entry -> game round
@@ -51,7 +167,14 @@ window.addEventListener("load", function (event) {
     roundSpan.innerText = `${round}`;
     round += 1;
     gameRound();
-  }, 1000);
+  }, 20);
+  // function main() {
+  //   roundSpan.innerText = `${round}`;
+  //   round += 1;
+  //   gameRound();
+  //   main();
+  // }
+  // setTimeout(main, 1000);
 
   // allow to click
   document.querySelectorAll("#seating div").forEach((element) => {
@@ -60,102 +183,13 @@ window.addEventListener("load", function (event) {
       let [row, col] = evt.target.getAttribute("id").split("_").map(Number);
       if (evt.target.className != "s") {
         evt.target.className = "s";
-        states[row][col] = 1;
-        // statesTemp[row][col] = 1;
+        liveStates[row][col] = 1;
+        updateCount(row, col);
       } else if (evt.target.className === "s") {
         evt.target.className = "a";
-        states[row][col] = 0;
-        // statesTemp[row][col] = 0;
+        liveStates[row][col] = 0;
+        updateCount(row, col, -1);
       }
     });
-    element.addEventListener(
-      "contextmenu",
-      (evt) => {
-        evt.preventDefault();
-        let [row, col] = evt.target.getAttribute("id").split("_").map(Number);
-        if (evt.target.className != "r") {
-          evt.target.className = "r";
-          states[row][col] = 2;
-          // statesTemp[row][col] = 2;
-        } else if (evt.target.className === "r") {
-          evt.target.className = "a";
-          states[row][col] = 0;
-          // statesTemp[row][col] = 0;
-        }
-      },
-      false
-    );
   });
-
-  // core
-  function gameRound() {
-    // update state
-    updateState();
-    // render
-    renderPage();
-    // save temp to state
-    states = statesTemp;
-    statesTemp = [];
-  }
-
-  function updateState() {
-    for (let r = 0; r < rowSize; r++) {
-      statesTemp.push([]);
-      for (let c = 0; c < colSize; c++) {
-        statesTemp[r].push(checkState(r, c));
-        // if (RANDOM && Math.random() < RANDOMPROB) {
-        //   statesTemp[r][c] = 1 - statesTemp[r][c];
-        // }
-      }
-    }
-  }
-
-  function checkState(r, c) {
-    let group1 = 0;
-    let group2 = 0;
-    for (let i = r - 1; i <= r + 1; i++) {
-      for (let j = c - 1; j <= c + 1; j++) {
-        if (i === r && j === c) {
-          continue;
-        }
-        let it = i < 0 ? rowSize - 1 : i >= rowSize ? 0 : i;
-        let jt = j < 0 ? colSize - 1 : j >= colSize ? 0 : j;
-        if (states[it][jt] === 1) group1++;
-        else if (states[it][jt] === 2) group2++;
-      }
-    }
-    if (states[r][c] === 1) {
-      // rule 1: dead if near-by live cell <= 1 (SPARSE)
-      if (group1 <= SPARSE) return 0;
-      // rule 2: dead if near-by live cell >= 4 (DENSE)
-      else if (group1 >= DENSE) return 0;
-      else if (group2 >= 2) return 0;
-      else return 1;
-    } else if (states[r][c] === 2) {
-      // rule 1: dead if near-by live cell <= 1 (SPARSE)
-      if (group2 <= 1) return 0;
-      // rule 2: dead if near-by live cell >= 4 (DENSE)
-      else if (group2 >= 3) return 0;
-      else return 2;
-    } else {
-      // rule 3: alive if near-by live cell == 3 (REPRODUCE)
-      if (group1 === REPRODUCE && group2 <= 0) return 1;
-      if (group2 === 3) return 2;
-      if (group2 === 2 && group1 >= 1) return 2;
-      else return 0;
-    }
-  }
-  function renderPage() {
-    for (let r = 0; r < rowSize; r++) {
-      for (let c = 0; c < colSize; c++) {
-        if (statesTemp[r][c] === 1) {
-          document.getElementById(`${r}_${c}`).className = "s";
-        } else if (statesTemp[r][c] === 2) {
-          document.getElementById(`${r}_${c}`).className = "r";
-        } else {
-          document.getElementById(`${r}_${c}`).className = "a";
-        }
-      }
-    }
-  }
 });
